68384dbbf44ab912cb35999645bfb5fef5e92f06
mauro-mandalorian's <mauro.santos@esx.com.br>
2023-10-18 16:17:23 -0300

    add try catch no todo repositorio

--- src/Pages/ToDo/ToDoRepository.jsx
+++ src/Pages/ToDo/ToDoRepository.jsx
@@ -3,7 +3,6 @@ import { api } from "../Login/LoginRepository";
 async function getAllTodos() {
   try {
     const response = await api.get('/todos?pageSize=5&pageNo=0&sortBy=todoId');
-    console.log("getAllTodos:", response.data);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -11,18 +10,30 @@ async function getAllTodos() {
 }
 
 const addTodo = async (task) => {
-  const response = await api.post('/todos/create', task);
-  return response.data;
+  try {
+    const response = await api.post('/todos/create', task);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const updateTodo = async (id, task) => {
-  const response = await api.put(`/todos/${id}`, task);
-  return response.data;
+  try {
+    const response = await api.put(`/todos/update/${id}`, task);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const deleteTodo = async (id) => {
-  const response = await api.delete(`/todos/${id}`);
-  return response.data;
+  try {
+    const response = await api.delete(`/todos/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export { addTodo, deleteTodo, getAllTodos, updateTodo };
