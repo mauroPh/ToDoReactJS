6eeab4f4d26a7bff672f81eb37b585bc7eee1b21
SouzaGo <122278392+SouzaGo@users.noreply.github.com>
2023-10-18 11:29:38 -0300

    Inserindo edição do todo

--- src/components/ToDoItemComponent.jsx
+++ src/components/ToDoItemComponent.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { deleteTodo } from "../Pages/ToDo/ToDoRepository";
 
 function ToDoItemComponent(props) {
   console.log("ToDoItemComponent props:", props);
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedTodoText, setUpdatedTodoText] = useState(props.todo.description);
+
+  function handleUpdate() {
+    const updatedTodo = { ...props.todo, description: updatedTodoText };
+    props.handleUpdate(props.id, updatedTodo);
+    setIsEditing(false);
+  }
+
+  function handleCancel() {
+    setIsEditing(false);
+    setUpdatedTodoText(props.todo.description);
+  }
 
   const handleDelete = async () => {
     try {
@@ -14,33 +27,35 @@ function ToDoItemComponent(props) {
   };
 
   return (
-    <div
-      className={
-        props.completed ? "todo-item todo-item-completed" : "todo-item"
-      }
-    >
+    <div className={props.completed ? "todo-item todo-item-completed" : "todo-item"}>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={updatedTodoText}
+            onChange={(event) => setUpdatedTodoText(event.target.value)}
+          />
+          <button onClick={handleUpdate}>Salvar</button>
+          <button onClick={handleCancel}>Cancelar</button>
+        </>
+      ) : (
+        <>
       <input
         type="checkbox"
         checked={props.completed}
         onChange={() => props.handleCheckboxChange(props.id)}
       />
 
-      <label
-        className={
-          props.completed ? "todo-label todo-label-completed" : "todo-label"
-        }
-      >
+      <label className={props.completed ? "todo-label todo-label-completed" : "todo-label"}>
         {props.todo.description}
       </label>
-
-      <button
-        className={
-          props.completed ? "delete-button delete-button-completed" : "delete-button"
-        }
-        onClick={handleDelete}
-      >
+      <button onClick={() => setIsEditing(true)}>Editar</button>
+      <button className={ props.completed ? "delete-button delete-button-completed" : "delete-button"}
+        onClick={handleDelete}>
         Remover
       </button>
+      </>
+      )}
     </div>
   );
 }
