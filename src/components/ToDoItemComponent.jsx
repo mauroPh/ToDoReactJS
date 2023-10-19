68f437c09d33056fde33f468cebb4e5f63a0ab0f
mauro-mandalorian's <mauro.santos@esx.com.br>
2023-10-19 00:57:27 -0300

    remove botao edicao abre com clique em description

--- src/components/ToDoItemComponent.jsx
+++ src/components/ToDoItemComponent.jsx
@@ -1,7 +1,7 @@
+import { mdiClose, mdiDeleteOutline } from '@mdi/js';
+import Icon from '@mdi/react';
 import React, { useState } from "react";
 import { deleteTodo } from "../Pages/ToDo/ToDoRepository";
-import Icon from '@mdi/react';
-import { mdiClose, mdiDeleteOutline, mdiPencilBoxOutline } from '@mdi/js';
 
 
 function ToDoItemComponent(props) {
@@ -30,37 +30,51 @@ function ToDoItemComponent(props) {
     }
   };
 
+  const handleDescriptionClick = () => {
+    setIsEditing(true);
+    setUpdatedTodoText(props.todo.description);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleUpdate();
+    }
+  };
+
   return (
     <div className={props.completed ? "todo-item todo-item-completed" : "todo-item"}>
       {isEditing ? (
-        <>
-          <input
-            type="text"
-            value={updatedTodoText}
-            onChange={(event) => setUpdatedTodoText(event.target.value)}
-          />
-          <button onClick={handleUpdate}>Salvar</button>
-          <button onClick={handleCancel}><Icon path={mdiClose} size={1} /></button>
-        </>
+        <div className="popup">
+          <div className="popup-content">
+            <textarea
+              value={updatedTodoText}
+              onChange={(event) => setUpdatedTodoText(event.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+            <button onClick={handleUpdate}>Salvar</button>
+            <button onClick={handleCancel}><Icon path={mdiClose} size={1} /></button>
+          </div>
+        </div>
       ) : (
         <>
-      <input
-        type="checkbox"
-        checked={props.completed}
-        onChange={() => props.handleCheckboxChange(props.id)}
-      />
-
-      <label className={props.completed ? "todo-label todo-label-completed" : "todo-label"}>
-        {props.todo.description}
-      </label>
-      <button onClick={() => setIsEditing(true)}><Icon path={mdiPencilBoxOutline} size={1.6} /></button>
-      <button className={ props.completed ? "delete-button delete-button-completed" : "delete-button"}
-        onClick={handleDelete}>
-        <Icon path={mdiDeleteOutline} size={1} />
-      </button>
-      </>
+          <input
+            type="checkbox"
+            checked={props.completed}
+            onChange={() => props.handleCheckboxChange(props.id)}
+          />
+          <label
+            className={props.completed ? "todo-label todo-label-completed" : "todo-label"}
+            onClick={handleDescriptionClick}
+          >
+            {props.todo.description}
+          </label>
+          <button className={props.completed ? "delete-button delete-button-completed" : "delete-button"} onClick={handleDelete}>
+            <Icon path={mdiDeleteOutline} size={1} />
+          </button>
+        </>
       )}
     </div>
   );
 }
+
 export default ToDoItemComponent;
