d7ca42905de1d2413ca2e14b0837b639b1177f9f
mauro-mandalorian's <mauro.santos@esx.com.br>
2023-11-08 00:30:57 -0300

    alteraçao text carousel

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -11,16 +11,23 @@ import Footer from '../../styles/styledComponents/Footer';
 const HomePage = () => {
     const items = [
         {
-            text: 'Sem uma boa organização, nos sentimos sobrecarregados e estressados,lutando para acompanhar nossas obrigações. Em resumo, a organização é a chave para uma vida mais equilibrada e produtiva.',
-            img: img1
+            title: 'Organização',
+            text: 'Sem uma boa organização, nos sentimos sobrecarregados e estressados, lutando para acompanhar nossas obrigações. A organização é a chave para uma vida mais equilibrada e produtiva.',
+            img: img1,
+            className: 'card-style-1'
         },
         {
+            title: 'To-do App',
             text: 'Um aplicativo de lista de tarefas, ou "to-do app", é uma ferramenta poderosa para colocar a organização em prática. Com ele, você pode criar e organizar suas tarefas de forma rápida e adaptável ao seu ritmo.',
-            img: img2
+            img: img2,
+            className: 'card-style-2'
         },
         {
+            title: 'Simplicidade',
             text: 'Em um mundo cada vez mais acelerado, a simplicidade é fundamental. Simplifique sua vida, mantendo as suas tarefas em um único lugar. Sem listas de papel ou anotações perdidas. Torne sua vida e a de sua equipe mais organizada e eficiente.',
-            img: img3
+            img: img3,
+            className: 'card-style-1'
+
         }
     ];
     return (
@@ -38,16 +45,19 @@ const HomePage = () => {
           </nav>
           <Carousel>
             {items.map((item, i) => (
-              <Carousel.Item interval={6000} key={i}>
-                <div className="card">
-                  <div className="card-body">
-                    <img className="d-block w-90 carousel-image" src={item.img} alt="Imagem do item" />
-                    <p className="card-text">
-                      {item.text}
-                    </p>
-                  </div>
-                </div>
-              </Carousel.Item>
+                 <Carousel.Item interval={7000} key={i}>
+                 <div className={`card ${item.className}`}>
+                   <div className="card-body">
+                   <img className="d-block w-90 carousel-image" src={item.img} alt="Imagem do item" />
+                     <div className="card-text-content">
+                       <h3>{item.title}</h3>
+                       <p >
+                       {item.text}
+                       </p>
+                     </div>
+                   </div>
+                 </div>
+               </Carousel.Item>
             ))}
           </Carousel>
           <Footer></Footer>
